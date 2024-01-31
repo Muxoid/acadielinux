ff269c24ead2873b337cc42a5d26834981c0ba24
muxoid <shawn.cormier@muxoid.com>
2024-01-31 18:16:43 -0400

    Added HTTPS in prod and HTTP port 3000 in dev.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
+const https = require('https');
 const path = require('path');
 const serveIndex = require('serve-index');
 
 const app = express();
-const port = 3000;
+const devPort = 3000;
+const prodPort = 443;
+
+const port = process.env.NODE_ENV === 'production' ? prodPort : devPort;
 
 // Setup Handlebars
 app.engine('handlebars', engine());
@@ -16,8 +20,8 @@ app.set('views', './views');
 const servePath = path.join(__dirname, 'public');
 
 // Serve static files and provide directory listing
-app.use('/arch', express.static(servePath));
-app.use('/arch', serveIndex(servePath, {'icons': true}));
+app.use('/mirrors', express.static(servePath));
+app.use('/mirrors', serveIndex(servePath, {'icons': true}));
 
 // Routes
 app.get('/', (req, res) => {
@@ -28,7 +32,22 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
+if (process.env.NODE_ENV === 'production') {
+// SSL certificate
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/mirror.acadielinux.ca/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/mirror.acadielinux.ca/cert.pem'),
+    ca: fs.readFileSync('/etc/letsencrypt/live/mirror.acadielinux.ca/chain.pem')
+};
+
+
 // Start server
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+https.createServer(options, app).listen(port, () => {
+    console.log(`HTTPS server running on https://localhost:${port}`);
 });
+} else {
+    // Development mode - HTTP
+    app.listen(devPort, () => {
+        console.log(`Server running on http://localhost:${devPort}`);
+    });
+}
